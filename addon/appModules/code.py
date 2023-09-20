3b4e490017e9ef3833bf2cd1388207af0d0a588f
DalisonCPU <dalisonmg@gmail.com>
2023-09-20 01:01:45 -0300

    Making the addon faster

--- addon/appModules/code.py
+++ addon/appModules/code.py
@@ -4,6 +4,7 @@ from scriptHandler import script
 import api
 
 class AppModule(appModuleHandler.AppModule):
+    sb = None
     @script(gesture="kb:NVDA+W", description="Report current line number in vscode.",category="vscode-line-numbers")
     def script_sayLineNumber(self, gesture):
         ui.message(self.getStatusInformation("line"))
@@ -13,11 +14,14 @@ class AppModule(appModuleHandler.AppModule):
         ui.message(self.getStatusInformation("col"))
 
     def getStatusInformation(self, mode):
-        sb = api.getStatusBar().children
+        if self.sb is None:
+
+            self.sb = api.getStatusBar().children
+
         if mode == "line":
-            return sb[-2].name.split(",")[0]
+            return self.sb[-2].name.split(",")[0]
         elif mode == "col":
-            return sb[-2].name.split(",")[1]
+            return self.sb[-2].name.split(",")[1]
 
     previousLine = None
 
